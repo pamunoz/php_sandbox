bb74c44df49c48a4108f066e69be42385769ebf9
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-22 15:12:06 -0300

    Add switch statements

---
+++ logical2.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Logical Expressions: Switch</title>
+    </head>
+    <body>
+        
+        <?php
+            $a = 3;
+            
+            switch ($a) {
+                case 0: 
+                    echo "a equals 0";
+                    break;
+                case 1: 
+                    echo "a equals 1";
+                    break;
+                case 2:
+                    echo "a equals 2";
+                    break;
+                default:
+                    echo "a is not 0, 1, or 2";
+            }
+        ?>
+    </body>
+</html> 
