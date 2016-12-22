b7fad8f1e11dfea8f0810631a6d519db8224291f
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-22 20:39:10 -0300

    Add more flexible functions

--- functions.php
+++ functions.php
@@ -13,6 +13,15 @@
                 echo "Hello {$word}!<br />";
             }
             say_hello2("Mother fucker");
+            
+            // using a already defined variable
+            $name = "John Doe";
+            say_hello2($name);
+            
+            function say_hello3($greeting, $target, $punctuation) {
+                echo $greeting . ", " . $target . $punctuation . "<br />";
+            }
+            say_hello3("Hello", $name, "!!!!!!!!")
         ?>
         <br/>
     </body>
