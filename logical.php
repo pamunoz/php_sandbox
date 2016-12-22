b3100e03d3584fbff64bec23cd94ee510e547a6e
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-22 15:07:14 -0300

    Add AND and OR

--- logical.php
+++ logical.php
@@ -14,7 +14,36 @@
             } else {
                 echo "a is smaler than b";
             }
-        ?><br/>
+        ?>
+        <br/>
+        <?php
+            $c = 1;
+            $d = 20;
+            if (($a > $b) && ($c > $d)) {
+                echo "a is larger than b AND";
+                echo "c is larger than d";
+            }
+            if (($a > $b) || ($c > $d)) {
+                echo "a is larger than b OR";
+                echo "c is larger than d";
+            } else {
+                echo "neither a is larger than b or c is larger than d";
+            }
+        ?>
+        <br/>
+        <?php
+            if (!isset($a)) {
+                $a = 100;
+            }
+            echo $a;
+        ?>
+        <br/>
+        <?php
+            if (is_int($a)) {
+                settype($a, "string");
+            }
+            echo gettype($a);
+        ?>
         
     </body>
 </html> 
