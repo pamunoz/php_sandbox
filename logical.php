a7f9d5418e34f0b5ab1e2c09df9f84bf7a241821
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-22 14:52:22 -0300

    Add if statements

---
+++ logical.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Logical Expressions</title>
+    </head>
+    <body>
+        
+        <?php
+            $a = 3;
+            $b = 4;
+            if ($a > $b) {
+                echo "a is larger than b";    
+            }
+        ?><br/>
+        
+    </body>
+</html> 
