ccf0adefffd57fe472c5605ee5022198e8781ac3
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-21 22:58:26 -0300

    Initial commit

---
+++ helloworld.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Hello World!</title>
+    </head>
+    <body>
+        
+        <?php 
+        // single-line comments are like this
+        # or like this
+        /* our double line commmet
+        here and we can keep typing and typing*/
+        echo "Hello World!"; ?><br/>
+        <?php print "Hello World!"; ?><br/>
+        <?php echo "Hello"." World!"; ?><br/>
+        <?php echo 2+3; ?><br/>
+
+
+    </body>
+</html>
