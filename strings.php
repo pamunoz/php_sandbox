a3f34245277366461ddbc70393434ff4d0d262a5
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-22 09:57:56 -0300

    Add string and variables

---
+++ strings.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Strings</title>
+    </head>
+    <body>
+        
+        <?php 
+            echo "Hello World<br />";
+            echo 'Hello World<br />';
+            $my_variable = "Hello World";
+            echo $my_variable;
+            echo "<br />";
+            echo $my_variable . " Again";
+        ?>
+    </body>
+</html>
---
+++ variables.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Variables</title>
+    </head>
+    <body>
+        
+        <?php 
+            $var1 = 10;
+            echo $var1;
+            $var2 = "Hello World";
+            echo $var2;
+        
+        ?>
+    </body>
+</html>
