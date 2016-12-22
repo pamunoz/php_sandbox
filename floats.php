49912932046be8b67f58358d8d22bbae6fc50cdf
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-22 11:03:13 -0300

    Add floats numbers

---
+++ floats.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Numbers: Floating Point Numbers</title>
+    </head>
+    <body>
+        
+        <?php $var1 = 3.14; ?>
+        The value of 4/3
+        <?php echo 4/3; ?>
+        
+        <br/>
+        Floating point <?php echo $myFloat = 3.14; ?><br />
+        Round: <?php echo round($myFloat, 1); ?><br />
+        Ceiling: <?php echo ceil($myFloat); ?><br />
+        Floor: <?php echo floor($myFloat); ?><br />
+        
+    </body>
+</html>
