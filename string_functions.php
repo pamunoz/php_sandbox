f581eca3988aea1fd9e04648969016c38daa40fd
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-22 10:21:01 -0300

    Add more self explanatory functions

--- string_functions.php
+++ string_functions.php
@@ -18,5 +18,11 @@
         Uppercase: <?php echo strtoupper($thirdString); ?><br />
         Uppercase first-letter: <?php echo ucfirst($thirdString); ?><br />
         Uppercase words: <?php echo ucwords($thirdString); ?><br />
+        <br/>
+        <!-- self explanatory functions -->
+        Length: <?php echo strlen($thirdString); ?><br />
+        Trim: <?php echo $fourthString = $firstString . trim($secondString); ?> <br/>
+        Find: <?php echo strstr($thirdString, "brown"); ?><br />
+        Replace by string: <?php echo str_replace("quick", "super-fast", $thirdString) ?><br />
     </body>
 </html>
