6743763a16dcd000f08244581040aa2b55485af7
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-22 14:55:43 -0300

    Add elleif and else statements

--- logical.php
+++ logical.php
@@ -9,6 +9,10 @@
             $b = 4;
             if ($a > $b) {
                 echo "a is larger than b";    
+            } elseif($a == $b) {
+                echo "a equals b";
+            } else {
+                echo "a is smaler than b";
             }
         ?><br/>
         
