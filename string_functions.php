f102402a6a16459cfc7712c72483acc6771d8ec0
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-22 10:28:32 -0300

    Add repeat, substring, find position and find character

--- string_functions.php
+++ string_functions.php
@@ -24,5 +24,10 @@
         Trim: <?php echo $fourthString = $firstString . trim($secondString); ?> <br/>
         Find: <?php echo strstr($thirdString, "brown"); ?><br />
         Replace by string: <?php echo str_replace("quick", "super-fast", $thirdString) ?><br />
+        <br />
+        Repeat: <?php echo str_repeat($thirdString, 2); ?><br />
+        Make substring: <?php echo substr($thirdString, 5, 10); ?><br />
+        Find position: <?php echo strpos($thirdString, "brown"); ?><br />
+        Find character: <?php echo strchr($thirdString, "z"); ?><br />
     </body>
 </html>
