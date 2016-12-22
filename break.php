8f09ca007f7e141f384a956081ac39662cc165cc
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-22 18:04:37 -0300

    Add break

---
+++ break.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Loops: break</title>
+    </head>
+    <body>
+        <?php
+            for ($count = 0; $count <= 10; $count++) {
+                echo $count;
+                if ($count == 10) {
+                    break;
+                }
+                echo ", ";
+            }
+        ?>
+        <br/>
+    </body>
+</html> 
