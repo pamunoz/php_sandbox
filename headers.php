69f4ea00bd56b2f81bd8414d096c8b596095f0e1
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-23 18:25:20 -0300

    Add headers and output buffering

---
+++ headers.php
@@ -0,0 +1,18 @@
+<?php
+    // This is how you redirect a page
+    header("Location: basic.html");
+    exit;
+?>
+<?php
+    // This is how you return a 404 error
+    //header("HTTP/1.0 404 Not Found");
+    //exit;
+?>
+<html>
+    <head>
+        <title>Headers</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
