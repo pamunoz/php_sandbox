f9315b0f378be9e8a33f99a7400015fcf99d5f5a
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-23 16:57:29 -0300

    Add form and procesing

---
+++ forms.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Forms</title>
+    </head>
+    <body>
+        <form action="process.php" method="post">
+            Username: <input type="text" name="username" value="" /><br />
+            Password: <input type="password" name="password" value="" /><br />
+            <input type="submit" name="submit" value="Submit" /><br />
+        </form>
+    </body>
+</html>
+
