b3d30c80a43f2911a985597f26268aa4cf5f19ee
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-22 21:12:37 -0300

    Add return values

--- functions.php
+++ functions.php
@@ -21,8 +21,26 @@
             function say_hello3($greeting, $target, $punctuation) {
                 echo $greeting . ", " . $target . $punctuation . "<br />";
             }
-            say_hello3("Hello", $name, "!!!!!!!!")
+            say_hello3("Hello", $name, "!!!!!!!!");
         ?>
         <br/>
+        <?php
+            function addition($val1, $val2) {
+                $sum = $val1 + $val2;
+                return $sum;
+            }
+            $new_val = addition(3, 4);
+            echo $new_val;
+            
+            function add_subt($val1, $val2) {
+                $add = $val1 + $val2;
+                $subt = $val1 - $val2;
+                $result = array($add, $subt);
+                return $result;
+            }
+            $result_array = add_subt(10, 5);
+            echo "Add: " . $result_array[0];
+            echo "Subst: " . $result_array[1];
+        ?>
     </body>
 </html>
