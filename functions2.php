f57c00dee40276902ffcebbb8df85994006267c2
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-22 21:41:03 -0300

    Add global variables

---
+++ functions2.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>Functions 2</title>
+    </head>
+    <body>
+        
+        <?php 
+            // using global
+            $bar = "outside";
+            function foo() {
+                global $bar;
+                $bar = "inside";
+            }
+            foo();
+            echo $bar . "<br />";
+        ?>
+        <br />
+        <?php 
+            // using local variables, arguments and return values
+            $bar = "outside";
+            function foo2($var) {
+                $var = "inside";
+                return $var;
+            }
+            $bar = foo2($bar);
+            echo $bar . "<br />";
+        ?>
+        
+        
+    </body>
+</html>
